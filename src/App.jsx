37af6486135b245c76495946781311660e019504
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-10-13 14:53:38 -0600

    carousel example

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import "./App.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Product from "./Product";
+import { productData, responsive } from "./data";
+
+export default function App() {
+  /* 
+display: flex;
+    position: absolute;
+    bottom: 12px;
+    padding: 18px;
+    left: 564px;
+    gap: 27px;
+*/
+
+  const ButtonGroup = ({ next, previous, goToSlide, ...rest }) => {
+    const {
+      carouselState: { currentSlide },
+    } = rest;
+    return (
+      <div className="carousel-button-group">
+        {" "}
+        <button
+          className={currentSlide === 0 ? "disable" : ""}
+          onClick={() => previous()}
+        />
+        <button onClick={() => next()} />
+        <button onClick={() => goToSlide(0)}> Go to any slide </button>
+      </div>
+    );
+  };
+
+  const product = productData.map((item) => (
+    <Product
+      name={item.name}
+      url={item.imageurl}
+      price={item.price}
+      description={item.description}
+    />
+  ));
+
+  return (
+    <div className="App">
+      <h1>React multi carousel</h1>
+      <Carousel
+        responsive={responsive}
+        swipeable
+        infinite
+        // autoPlay
+        // autoPlaySpeed={6000}
+        transitionDuration={600}
+        removeArrowOnDeviceType={["tablet", "mobile", "superLargeDesktop"]}
+        customButtonGroup={<ButtonGroup />}
+      >
+        {product}
+      </Carousel>
+    </div>
+  );
+}
